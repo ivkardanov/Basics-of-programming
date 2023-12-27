882d76dffab32ae7c5484c40f11395ed0aab8e5a
ivkardanov <146481636+ivkardanov@users.noreply.github.com>
2023-12-27 15:56:47 +0300

    Add files via upload

---
+++ graphs (lab 3 and lab  4)/gr1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+int maxPathSum(TreeNode* root, int& maxSum) {
+    if (root == nullptr) {
+        return 0;
+    }
+
+    int leftMax = max(0, maxPathSum(root->left, maxSum));
+    int rightMax = max(0, maxPathSum(root->right, maxSum));
+
+    int currentMax = root->val + leftMax + rightMax;
+    maxSum = max(maxSum, currentMax);
+
+    return root->val + max(leftMax, rightMax);
+}
+
+int main() {
+    string choice;
+    do {
+        // Создаем дерево из примера [-10,9,20,null,null,15,7]
+        TreeNode* root = new TreeNode{ -10 };
+        root->left = new TreeNode{ 9 };
+        root->right = new TreeNode{ 20 };
+        root->right->left = new TreeNode{ 15 };
+        root->right->right = new TreeNode{ 7 };
+
+        int maxSum = INT_MIN;
+        int result = maxPathSum(root, maxSum);
+        cout << "Max path sum: " << result << endl;
+
+        // Очищаем память, выделенную под дерево
+        delete root->right->right;
+        delete root->right->left;
+        delete root->right;
+        delete root->left;
+        delete root;
+
+        cout << "Run again ? (y/n): ";
+        cin >> choice;
+    } while (choice == "Y" || choice == "y");
+    return 0;
+}
---
+++ graphs (lab 3 and lab  4)/gr3.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void addNum(vector<int>& counter, int& totalNumbers, int num) {
+    counter[num]++;
+    totalNumbers++;
+}
+
+double findMedian(const vector<int>& counter, int totalNumbers) {
+    int count = 0;
+    int median1 = -1, median2 = -1;
+
+    for (int i = 0; i <= 100; i++) {
+        count += counter[i];
+        if (median1 == -1 && count >= (totalNumbers + 1) / 2) {
+            median1 = i;
+        }
+        if (count >= (totalNumbers + 2) / 2) {
+            median2 = i;
+            break;
+        }
+    }
+
+    if (totalNumbers % 2 == 0) {
+        return (median1 + median2) / 2.0;
+    }
+    else {
+        return median2;
+    }
+}
+
+int main() {
+    string choice;
+    do {
+        vector<int> counter(101, 0);
+        int totalNumbers = 0;
+
+        int arr[] = { 1,2,3,4,5 };
+        int n = sizeof(arr) / sizeof(arr[0]);
+
+        for (int i = 0; i < n; i++) {
+            addNum(counter, totalNumbers, arr[i]);
+        }
+
+        cout << "Median is: " << findMedian(counter, totalNumbers) << std::endl;
+        cout << "Run again? (y/n): ";
+        cin >> choice;
+    } while (choice == "y" || choice == "Y");
+    return 0;
+}
